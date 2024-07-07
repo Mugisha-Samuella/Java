1de84d9750682fb24cd8271b1f1ccd5b3c184ce9
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 01:38:11 +0300

    Print New Line

---
+++ src/NewLine.java
@@ -0,0 +1,10 @@
+public class NewLine {
+    public static void main(String[] args) {
+        System.out.println("Hey! \n How are you?");
+
+        int age = 25;
+        int salary = 5000;
+
+        System.out.println("My age is: " + age + ". \n My Salary is: " + salary);
+    }
+}
