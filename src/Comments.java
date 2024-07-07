ded002dbe0de0cb7c52f07372b5df0041a58c1d2
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-07 23:54:29 +0300

    Comments

---
+++ src/Comments.java
@@ -0,0 +1,7 @@
+public class Comments {
+    public static void main(String[] args) {
+        //We use // to write comments
+        //Prints Hello world to the screen
+        System.out.println("Hello World"); //This is another comment on the same line with codes;
+    }
+}
