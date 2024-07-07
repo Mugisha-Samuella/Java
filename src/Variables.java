7346852315986a1118e3774d83fcb4b23419f04b
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-07 13:39:08 +0200

    Variable declaration and initialisation

--- src/Variables.java
+++ src/Variables.java
@@ -1,7 +1,10 @@
 public class Variables {
+    static int height;
+
     public static void main(String[] args){
         int age = 30;
+        int height = 170;
 
-        System.out.println("I am " + age + " years old.");
+        System.out.println("I am " + age + " years old." + " I am " + height + " cm  tall." );
     }
 }
