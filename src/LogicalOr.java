2fa9d3cb79f30949c22c773a666756c667647422
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-07 15:11:15 +0200

    Logical OR(||)

---
+++ src/LogicalOr.java
@@ -0,0 +1,12 @@
+public class LogicalOr {
+    public static void main(String[] args) {
+
+        //checks if one is a teacher or a student and
+        //returns true when least one bool result == true and false otherwise.
+
+        boolean isTeacher = true;
+        boolean isStudent = true;
+
+        System.out.println(isTeacher || isStudent);
+    }
+}
