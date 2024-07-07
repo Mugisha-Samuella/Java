d73391e1032c76b0fb159f1ae1ec0354c318ef3c
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 02:08:51 +0300

    Operators (Instanceof)

---
+++ src/InstanceOf.java
@@ -0,0 +1,8 @@
+public class InstanceOf {
+    public static void main(String[] args) {
+        String str = "Is str an instance of String? ";
+        boolean result = str instanceof String;
+
+        System.out.println(str + result);
+    }
+}
