8d97b21997f22dde73bb3d984668bf543b9dfce5
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-07 15:03:16 +0200

    Logical And(&&)

---
+++ src/LogicalAnd.java
@@ -0,0 +1,10 @@
+public class LogicalAnd {
+    public static void main(String[] args){
+        int age = 25;
+
+        //check if age is greater than or equal to 18
+        // and less than or equal to 40
+
+        System.out.println(age >= 18 && age <= 40); //true
+    }
+}
