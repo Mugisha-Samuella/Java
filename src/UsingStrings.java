6f259885538d773f21bba6dc69e3fd06809c7f15
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 23:26:21 +0300

    Strings - Literal and Object

---
+++ src/UsingStrings.java
@@ -0,0 +1,7 @@
+public class UsingStrings {
+    public static void main(String[] args) {
+        String literalString = "Hello world!"; //A literal String
+
+        String ObjectString = new String("Hello World"); //This is an Object String
+    }
+}
