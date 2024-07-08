f1ee62158998b00682c179b179fe889e5c7ac3fa
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 23:52:47 +0300

    Strings - isEmpty

--- src/StringInstanceProperty.java
+++ src/StringInstanceProperty.java
@@ -3,5 +3,8 @@ public class StringInstanceProperty {
         String school = "Rwanda Coding Academy";
 
         System.out.println(school.length()); //Gets the length of the string (Whitespaces included)
+
+        //Checks if String is empty
+        System.out.println(school.isEmpty()); //output is false
     }
 }
