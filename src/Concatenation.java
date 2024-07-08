9b03365f621deb48ed0113539dfc29316dda45d7
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 23:32:58 +0300

    Concatenation

---
+++ src/Concatenation.java
@@ -0,0 +1,9 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        String name = "Mugisha Samuella";
+        String country = "Rwanda";
+        int age = 17;
+
+        System.out.println("Hello! I am " + name + " and I'm from " + country + ". I am " + age  + " years old." );
+    }
+}
