96f2005598028bd8708776961fcee53191cccd4f
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 23:39:27 +0300

    Formatted Strings

--- src/Concatenation.java
+++ src/Concatenation.java
@@ -2,8 +2,12 @@ public class Concatenation {
     public static void main(String[] args) {
         String name = "Mugisha Samuella";
         String country = "Rwanda";
-        int age = 17;
+        int age = 92;
+        String school = "Rwanda Coding Academy";
 
-        System.out.println("Hello! I am " + name + " and I'm from " + country + ". I am " + age  + " years old." );
+        String formattedString = String.format("Hi! I am %s and I'm from %s. I am %d years old and I currently study at %s", name, country, age, school);
+        System.out.println(formattedString);
+
+        //System.out.println("Hello! I am " + name + " and I'm from " + country + ". I am " + age  + " years old." );
     }
 }
