79954c0380de882a688f1daf8f3e1672f6756e4e
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 23:44:30 +0300

    Formatted Strings - More format Identifiers

--- src/Concatenation.java
+++ src/Concatenation.java
@@ -2,10 +2,14 @@ public class Concatenation {
     public static void main(String[] args) {
         String name = "Mugisha Samuella";
         String country = "Rwanda";
-        int age = 92;
+        int age = 92; //not real
         String school = "Rwanda Coding Academy";
+        double gpa = 4.0; //This is not real
+        char percentSign = '%';
+        boolean ImTellingTheTruth = false;
 
-        String formattedString = String.format("Hi! I am %s and I'm from %s. I am %d years old and I currently study at %s", name, country, age, school);
+
+        String formattedString = String.format("Hi! I am %s and I'm from %s. I am %d years old and I currently study at %s. My gpa is %f. I have 100%c in each subject and am I telling the truth? %b", name, country, age, school, gpa, percentSign, ImTellingTheTruth);
         System.out.println(formattedString);
 
         //System.out.println("Hello! I am " + name + " and I'm from " + country + ". I am " + age  + " years old." );
