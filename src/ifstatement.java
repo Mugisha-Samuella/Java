0eb2a9cf6365385d541c799c1f5eaf987c41dbcf
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-10 23:23:24 +0300

    If Statement - String

--- src/ifstatement.java
+++ src/ifstatement.java
@@ -1,10 +1,17 @@
 public class ifstatement {
     public static void main(String[] args) {
         int number = 10;
+        String language = "Java";
 
+        //checks if number is positive or negative
         if (number < 0){
             System.out.println("Number is negative");
         }
         System.out.println("Number is positive");
+
+        //checks if language is Java
+        if(language == "Java"){
+            System.out.println(language + " is the best programming language");
+        }
     }
 }
