8afb98c8d228ef64c60f8d4a2c8b3b87d5936283
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-25 00:00:03 +0200

    -

--- src/Database.java
+++ src/Database.java
@@ -45,6 +45,13 @@ public class Database {
                 }
         System.out.println(name);
 
-        
+                try {
+                    int array[] = new int[10];
+                    array[10] = 30 / 0;
+                } catch (ArithmeticException e) {
+                    System.out.println(e.getMessage());
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println(e.getMessage());
+                }
     }
 }
