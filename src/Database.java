976d2e00d14e2da0c1c5c0b8790f408c688b1ed4
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-29 15:41:32 +0200

    [Deleted]

--- src/Database.java
+++ src/Database.java
@@ -44,14 +44,5 @@ public class Database {
                     throw new RuntimeException(e);
                 }
         System.out.println(name);
-
-                try {
-                    int array[] = new int[10];
-                    array[10] = 30 / 0;
-                } catch (ArithmeticException e) {
-                    System.out.println(e.getMessage());
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println(e.getMessage());
-                }
     }
 }
